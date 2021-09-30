8ef1070a20c7dce46f31871e48ad4787fea68fa1
TaiNguyen <tai@useget.com>
2021-09-30 22:07:11 +0700

    bk

---
+++ readme.md
@@ -0,0 +1,2 @@
+# source
+https://fullstack.edu.vn/blog/phan-1-tao-du-an-reactjs-voi-webpack-va-babel.html
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <h2>ok</h2>
+      <h3>la</h3>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
